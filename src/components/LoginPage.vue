0947a7c8d01bcc965c5b33e6f7b3c8cd5c83ab6e
Andrea Greco <andreag.email@gmail.com>
2023-09-24 21:15:07 +0700

    lg

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue + Vuex - User Registration and Login Example & Tutorial</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" />
+    <style>
+        a { cursor: pointer; }
+    </style>
+</head>
+<body>
+    <template>
+  <div>
+    <h1>Login</h1>
+
+    <form action="/login" method="POST">
+      <input type="email" name="email" placeholder="Email">
+      <input type="password" name="password" placeholder="Password">
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+
+</body>
+</html>
