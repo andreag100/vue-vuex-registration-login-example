77f3a45a80062f4df777cdf4f3a352bdac8e19c3
Andrea Greco <andreag.email@gmail.com>
2023-09-24 21:09:38 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     productionTip: false,
     database: {
-        process.env.DB_HOST = 'PLANETSCALE_HOST';
-        port: process.env.DB_PORT = '3306';
-        user: process.env.DB_USER = 'PLANETSCALE_USERNAME';
-        password: process.env.DB_PASSWORD = 'PLANETSCALE_PASSWORD';
-        name: process.env.DB_NAME = 'PLANETSCALE_NAME';
+        host: 'PLANETSCALE_HOST',
+        port: '3306',
+        user: 'PLANETSCALE_USERNAME',
+        password: 'PLANETSCALE_PASSWORD',
+        name: 'PLANETSCALE_NAME'    
     }
   }
